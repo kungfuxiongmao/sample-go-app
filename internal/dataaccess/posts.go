7a5d2b62d88f7a43645de586d0b4b9a2c2140ee0
kungfuxiongmao <jiahang.zhu2017@gmail.com>
2026-01-22 22:17:26 +0800

    Create Post Handler

--- internal/dataaccess/posts.go
+++ internal/dataaccess/posts.go
@@ -1,13 +1,21 @@
 package dataaccess
 
+import (
+	"time"
+
+	"github.com/kungfuxiongmao/sample-go-app/internal/models"
+)
+
 type CreatePost struct {
-	Name    string `json:"name"`
-	TopicID uint   `json:"topicID"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	TopicID     uint   `json:"topicID"`
 }
 
 type UpdatePost struct {
-	Name string `json:"updatedName"`
-	ID   uint   `json:"postID"`
+	Name        string `json:"updatedName"`
+	Description string `json:"description"`
+	ID          uint   `json:"postID"`
 }
 
 type DeletePost struct {
@@ -19,5 +27,13 @@ type FindPost struct {
 }
 
 // Create Return Structure
-type ReturnPost struct {
+type PostResponse struct { //for GET Methods
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	ID          uint        `json:"postID"`
+	User        models.User `json:"user"`
+	CreatedBy   uint
+	TopicID     uint
+	DateCreated time.Time `gorm:"autoCreateTime" json:"createdon"`
+}
 }
